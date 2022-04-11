3a78bc28f353f45a9e1b8f1c7034eb2af0ae0f8a
Susan Shrestha <susanshrestha2057@gmail.com>
2022-04-11 21:36:33 +0545

    array for coordinates

--- api/Location.js
+++ api/Location.js
@@ -4,9 +4,10 @@ const router = express.Router();
 //mongodb user model
 const User = require("./..//models/User");
 
-router.get("/donors", (req, res, next) => {
-    const { coordinates, distance, bloodGroup } = req.body;
-    const { latitude, longitude } = coordinates;
+router.get("/donors", async (req, res, next) => {
+    const { bloodGroup, coordinates, distance } = req.body;
+    const [latitude, longitude] = coordinates;
+
     const options = {
         isDonor: true,
         isAvailable: true,
@@ -18,9 +19,27 @@ router.get("/donors", (req, res, next) => {
         },
     };
 
-    User.find(options).then((data) => {
-        res.send(data);
-    });
+    await User.find(options)
+        .then((data) => {
+            if (data.length) {
+                res.json({
+                    status: "SUCCESS",
+                    message: "Donors found",
+                    data: data,
+                });
+            } else {
+                res.json({
+                    status: "FAILED",
+                    message: "No donors found.",
+                });
+            }
+        })
+        .catch((error) =>
+            res.json({
+                status: "FAILED",
+                message: "An error occured while searching donors.",
+            })
+        );
 });
 
 module.exports = router;
